045a5734670d9385795a5e19fadc16d3caa265b7
unknown <harold.gueneau@gmail.com>
2021-02-04 09:34:32 +0100

    indentation configurators

--- launcher_agent_training.py
+++ launcher_agent_training.py
@@ -14,41 +14,41 @@ state_size = 3
 action_size = 1
 
 actor_config = MultiLayersPerceptronConfigurator(
-                                                input_size = state_size,
-                                                output_size = action_size, 
-                                                hidden_layers = [64, 128, 64], 
-                                                learning_rate = 1e-3, 
-                                                hidden_activation = 'elu', 
-                                                activation_output = 'tanh'
-                                                )
+                input_size = state_size,
+                output_size = action_size, 
+                hidden_layers = [64, 128, 64], 
+                learning_rate = 1e-3, 
+                hidden_activation = 'elu', 
+                activation_output = 'tanh'
+                )
 
 critic_1_config = MultiLayersPerceptronConfigurator(
-                                                    input_size = state_size + action_size,
-                                                    output_size = 1, 
-                                                    hidden_layers = [64, 128, 64], 
-                                                    learning_rate = 5e-3, 
-                                                    hidden_activation = 'elu', 
-                                                    activation_output = 'linear'
-                                                    )
+                    input_size = state_size + action_size,
+                    output_size = 1, 
+                    hidden_layers = [64, 128, 64], 
+                    learning_rate = 5e-3, 
+                    hidden_activation = 'elu', 
+                    activation_output = 'linear'
+                    )
 
 critic_2_config = MultiLayersPerceptronConfigurator(
-                                                    input_size = state_size + action_size,
-                                                    output_size = 1, 
-                                                    hidden_layers = [64, 128, 64], 
-                                                    learning_rate = 5e-3, 
-                                                    hidden_activation = 'elu', 
-                                                    activation_output = 'linear'
-                                                    )
+                    input_size = state_size + action_size,
+                    output_size = 1, 
+                    hidden_layers = [64, 128, 64], 
+                    learning_rate = 5e-3, 
+                    hidden_activation = 'elu', 
+                    activation_output = 'linear'
+                    )
 
 agent_config = AgentConfigurator(
-                                memory_size = 100000,
-                                batch_size = 128, 
-                                gamma = 0.99, 
-                                tau = 1e-3, 
-                                action_min = -2.0, 
-                                action_max = 2.0,
-                                freq_update_actor = 5
-                                )
+                memory_size = 100000,
+                batch_size = 128, 
+                gamma = 0.99, 
+                tau = 1e-3, 
+                action_min = -2.0, 
+                action_max = 2.0,
+                freq_update_actor = 5
+                )
 
 agent = AgentTD3(agent_config, actor_config, critic_1_config, critic_2_config)
 
