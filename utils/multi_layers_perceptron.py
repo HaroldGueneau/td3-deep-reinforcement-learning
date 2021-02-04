521a128c4d07588b4391ce2d7622ec4a0d8ab28b
unknown <harold.gueneau@gmail.com>
2021-02-04 09:35:27 +0100

    indentation in mlp

--- utils/multi_layers_perceptron.py
+++ utils/multi_layers_perceptron.py
@@ -11,14 +11,20 @@ class MultiLayerPerceptron(nn.Module):
 
     def __init__(self, mlp_configurator: MultiLayersPerceptronConfigurator):
         super(MultiLayerPerceptron, self).__init__()
-        self.hidden_layers = self.__build_hidden_layers(hidden_layers=mlp_configurator.hidden_layers, hidden_activation=mlp_configurator.hidden_activation, input_size=mlp_configurator.input_size)
-        self.output_layer = Layer(input_size=mlp_configurator.hidden_layers[-1], output_size=mlp_configurator.output_size, activation=mlp_configurator.activation_output)
+        self.hidden_layers = self.__build_hidden_layers(hidden_layers=mlp_configurator.hidden_layers, 
+                                                        hidden_activation=mlp_configurator.hidden_activation, 
+                                                        input_size=mlp_configurator.input_size)
+        self.output_layer = Layer(input_size=mlp_configurator.hidden_layers[-1], 
+                                    output_size=mlp_configurator.output_size, 
+                                    activation=mlp_configurator.activation_output)
 
     def __build_hidden_layers(self, hidden_layers: list, hidden_activation: str, input_size: int):
         hidden_layers_modules = nn.ModuleList([])
         previous_hidden_size = input_size
         for hidden_size in hidden_layers:
-            hidden_layers_modules.append(Layer(input_size=previous_hidden_size, output_size=hidden_size, activation=hidden_activation))
+            hidden_layers_modules.append(Layer(input_size=previous_hidden_size, 
+                                                output_size=hidden_size, 
+                                                activation=hidden_activation))
             previous_hidden_size = hidden_size
         return hidden_layers_modules
 
